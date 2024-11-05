09c20f5d910d8dd56c1e1d79a7894d17e6521833
nirbhayVaghela92 <nirbhayVaghela92@gmail.com>
2024-11-05 11:30:29 +0530

    try to solve bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Handles client-side routing for SPA in development
-  },
+  base: './',
 })
